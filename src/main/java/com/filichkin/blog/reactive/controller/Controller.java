b668feca2cfb1a0b2362ff1f4e345c7c4aa136a7
Aleksandr_Filichkin <filia.aleks@gmail.com>
2019-08-31 15:29:42 +0300

    Microservice v3

--- src/main/java/com/filichkin/blog/reactive/controller/Controller.java
+++ src/main/java/com/filichkin/blog/reactive/controller/Controller.java
@@ -3,6 +3,8 @@ package com.filichkin.blog.reactive.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.net.URI;
@@ -15,19 +17,29 @@ import java.util.concurrent.Executors;
 @RestController
 public class Controller {
     private static final String USER_SERVICE = "http://ec2-34-201-131-104.compute-1.amazonaws.com:8080/user?delay=100";
-    private final HttpClient httpClient = HttpClient.newBuilder().executor(Executors.newFixedThreadPool(8)).build();
+//    private final HttpClient httpClient = HttpClient.newBuilder().executor(Executors.newFixedThreadPool(8)).build();
+    private final WebClient webClient= WebClient.builder().build();
 
-
-//    @GetMapping("user")
+    //    @GetMapping("user")
 //    public String getUser() throws IOException, InterruptedException {
 //        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
 //        return httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
 //    }
+//    @GetMapping("user")
+//    public CompletableFuture<String> getUser(){
+//        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
+//        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(httpResponse ->httpResponse.body());
+//    }
     @GetMapping("user")
-    public CompletableFuture<String> getUser(){
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
-        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(httpResponse ->httpResponse.body());
+    public Mono<String> getUser() {
+//        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
+//        return Mono.fromFuture(httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(httpResponse -> httpResponse.body()));
+        return webClient.get().uri(URI.create(USER_SERVICE))
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(x->"webclient mono "+x);
     }
 
 
