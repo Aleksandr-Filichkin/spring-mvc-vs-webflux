fa0072008608cc0a35424e9b03d192a433cda628
Aleksandr_Filichkin <filia.aleks@gmail.com>
2019-08-31 15:15:15 +0300

    Microservice v2

--- src/main/java/com/filichkin/blog/reactive/controller/Controller.java
+++ src/main/java/com/filichkin/blog/reactive/controller/Controller.java
@@ -9,17 +9,25 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 @RestController
 public class Controller {
     private static final String USER_SERVICE = "http://ec2-34-201-131-104.compute-1.amazonaws.com:8080/user?delay=100";
-    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final HttpClient httpClient = HttpClient.newBuilder().executor(Executors.newFixedThreadPool(8)).build();
 
 
+//    @GetMapping("user")
+//    public String getUser() throws IOException, InterruptedException {
+//        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
+//        return httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+//    }
     @GetMapping("user")
-    public String getUser() throws IOException, InterruptedException {
+    public CompletableFuture<String> getUser(){
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(USER_SERVICE)).build();
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(httpResponse ->httpResponse.body());
     }
 
 
